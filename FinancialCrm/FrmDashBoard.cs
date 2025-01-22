66dab59642f760c473e1652b861b6dfa06f8c862
Minnuree <gminnure@gmail.com>
2025-01-22 15:43:24 +0300

    Add project files.

---
+++ FinancialCrm/FrmDashBoard.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using FinancialCrm.Models;
+namespace FinancialCrm
+{
+    public partial class FrmDashboard : Form
+    {
+        public FrmDashboard()
+        {
+            InitializeComponent();
+        }
+
+        FinancialCrmDbEntities db = new FinancialCrmDbEntities();
+        int count = 0;
+        private void FrmDashboard_Load(object sender, EventArgs e)
+        {
+            var totalBalance = db.Banks.Sum(x => x.BankBalance);
+            lblTotalBalance.Text = totalBalance.ToString() + "₺";
+
+            var lastAmount = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(1).Select(y => y.Amount).FirstOrDefault();
+            lblLastBankProcessAmount.Text = lastAmount.ToString() + "₺";
+
+            // Chart 1 Kodları
+            var bankdata = db.Banks.Select(x => new
+            {
+                x.BankTitle,
+                x.BankBalance
+            }).ToList();
+
+            chart1.Series.Clear();
+            var series = chart1.Series.Add("Series1");
+
+            foreach(var item in bankdata)
+            {
+                series.Points.AddXY(item.BankTitle, item.BankBalance);
+            }
+            // Chart 2 Kodları
+            var billData = db.Bills.Select(x => new
+            {
+                x.BillTitle,
+                x.BillAmount
+            }).ToList();
+            chart2.Series.Clear();
+
+            var series2 = chart2.Series.Add("Faturalar");
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            foreach (var item in billData)
+            {
+                series2.Points.AddXY(item.BillTitle, item.BillAmount);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            if(count %4 == 1)
+            {
+                var electricBill = db.Bills.Where(x => x.BillTitle == "Elektrik Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "Elektrik Faturası";
+                lblBillAmount.Text = electricBill.ToString() + "₺";
+            }
+
+            if (count % 4 == 2)
+            {
+                var dogalgazBill = db.Bills.Where(x => x.BillTitle == "Doğalgaz Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "Doğalgaz Faturası";
+                lblBillAmount.Text = dogalgazBill.ToString() + "₺";
+            }
+
+            if (count % 4 == 3)
+            {
+                var suBill = db.Bills.Where(x => x.BillTitle == "Su Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "Su Faturası";
+                lblBillAmount.Text = suBill.ToString() + "₺";
+            }
+
+            if (count % 4 == 0)
+            {
+                var internetBill = db.Bills.Where(x => x.BillTitle == "İnternet Faturası").Select(y => y.BillAmount).FirstOrDefault();
+                lblBillTitle.Text = "İnternet Faturası";
+                lblBillAmount.Text = internetBill.ToString() + "₺";
+            }
+
+        }
+    }
+}
